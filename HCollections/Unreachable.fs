b25ce8a220e8d546279939de07ef3e696bde8310
Nicholas Cowle <nickcowle@gmail.com>
2019-03-31 17:43:53 +0100

    Make Unreachable internal

--- HCollections/Unreachable.fs
+++ HCollections/Unreachable.fs
@@ -3,4 +3,4 @@
 /// Exception used to indicate that a particular branch of
 /// code is impossible to hit, where the type system is
 /// unable to figure this out.
-exception Unreachable
+exception internal Unreachable
